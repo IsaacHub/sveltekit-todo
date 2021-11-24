ca0d6dfa576afd332a9dae3698073b506ce80493
IsaacHub <isaacselwin@gmail.com>
2021-11-25 00:25:28 +0530

    added reset css, fine tuning tsconfig

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,13 +9,52 @@ const config = {
 		preprocess({
 			scss: {
 				prependData: '@import "src/variables.scss";'
-			}
+			},
+      postcss: {
+        plugins: [require('autoprefixer')({ grid: 'autoplace'})]
+      },
+      babel: {
+        extensions: ['.js', '.mjs', '.html', '.ts', '.svelte'],
+        babelHelpers: 'runtime',
+        runtimeHelpers: true,
+        exclude: ['node_modules/@babel/**'],
+        plugins: [
+          '@babel/plugin-syntax-dynamic-import',
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              useESModules: true
+            }
+          ]
+        ],
+
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              loose: true,
+              // No need for babel to resolve modules
+              modules: false,
+              targets: {
+                // ! Very important. Target es6+
+                esmodules: true
+              }
+            }
+          ]
+        ]
+      }
 		})
 	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
 
 		vite: {
 			css: {
